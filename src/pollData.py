3356e9adaf987a6a6dd012665ece4ebe322afc57
Michael Weston <72511689+Nast33Narwhal@users.noreply.github.com>
2022-05-23 19:19:28 -0700

    Update pollData.py

--- src/pollData.py
+++ src/pollData.py
@@ -10,7 +10,9 @@ from smbus import SMBus
 import subprocess
 import numpy as np
 import math
-from scipy.io.wavfile import write 
+from scipy.io.wavfile import write
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
 
 filename = "test.wav"
 channels = 1
@@ -22,17 +24,24 @@ bus = SMBus(1)
 data1 = [0x85, 0x83]
 raw = 0
 out = 0
-while(1):
-    for x in range(50):
-        a = bus.write_i2c_block_data(0x48, 0x01, data1)
-        time.sleep(.1)
-        b = bus.read_i2c_block_data(0x48, 0x00, 2)
-        raw = b[0]*256 + b[1]    
-        out = raw * .0000625
-        #print(out)
-        adcData[x] = out;
-    adcData = np.array(adcData)
-    write("Fz.wav",sample_rate,adcData.astype(np.int16))
-    
+#while(1):
+for x in range(1000):
+    a = bus.write_i2c_block_data(0x48, 0x01, data1)
+    time.sleep(.01)
+    b = bus.read_i2c_block_data(0x48, 0x00, 2)
+    raw = b[0]*256 + b[1]    
+    out = raw * .0000625
+    #print(out)
+    adcData[x] = out;
+adcData = np.array(adcData)
+write("Fz.wav",sample_rate,adcData.astype(np.int16))
+fs,data = read(Fz.wav)
+N = len(data)
+ts = 1/fs
+t = np.arange(0,ts*N,ts)
+plt.plot(t,data)
+plt.show()
+
+
 #../res/data/yasaDatasets/
 bus.close()
