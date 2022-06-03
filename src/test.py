c95cfa57cd2d821c3743d68d9b93bb8ed8dd8c75
Nick Porter <nip2@pdx.edu>
2022-06-02 22:18:14 -0700

    replaced image, added test fcn

--- src/test.py
+++ src/test.py
@@ -2,21 +2,104 @@ import yasa
 from buildRaw import buildRawFromArray
 from scipy.io.wavfile import read
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from mne.filter import filter_data
+from testPlots import spindlePlot
 
+# defined functions
+# mov avg to smooth plot
+def moving_avg(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+def applyFilt(data,w):
+    N = len(data)
+    v = np.zeros(N)
+    g = moving_avg(data,w)
+    if len(g) <= N:
+        v[:len(g)] = g
+    else:
+        v = g[:N]
+    return v
+
+#OpenBCI Raw EEG Data
+#Number of channels = 8
+#Sample Rate = 250 Hz
+#Board = OpenBCI_GUI$BoardCytonSerial
+
+# read from our recorded data
 filepath = 'res/data/openBCI_Fz.wav'
 #filepath = '../res/data/yasaDatasets/Fz.wav'
 fs, data = read(filepath)
+data = data.astype(float)
 raw = buildRawFromArray(fs,data)
 
+# read from yasa example data
 #filepath = 'res/data/yasaDatasets/sub-02_mne_raw.fif'
 #raw = mne.io.read_raw_fif(filepath, preload=True, verbose=False)
-print('The channels are:', raw.ch_names)
-print('The sampling frequency is:', raw.info['sfreq'])
 
 sls = yasa.SleepStaging(raw, eeg_name="Fz")
+y_pred = sls.predict()
 y_proba = sls.predict_proba()
 confidence = y_proba.max(1)
-print(confidence)
-print(y_proba)
+#print(y_proba['N2'])
+print(y_pred)
 
-print(y_proba['N2'])
+# filter
+# threshold 1: sigma band detection
+# Broadband (1 - 40 Hz) bandpass filter
+freq_broad = (1, 30)
+filtered = filter_data(data, fs, freq_broad[0], freq_broad[1], method='fir',verbose=0)
+
+# filter for plotting
+y = applyFilt(filtered,15)
+
+# choose epoch to plot
+epoch = 20
+pred = y_pred[epoch]
+Nstart = fs * 30 * epoch
+Nend = Nstart + (fs * 30)
+segment = y[Nstart:Nend]
+ts = 1/fs
+N = len(segment)
+t = np.arange(0,N*ts,ts)
+
+#segment = np.where(abs(y) <= 0.2, y, 0)
+
+# times series plot
+#fig, ax = plt.subplots(figsize=(10,4))
+#ax.set_title('EEG time series with corresponding sleep stages')
+#ax.set_xlabel('time (s)')
+#ax.set_ylabel('Amplitude')
+
+#ax.plot(t,segment)
+#ax.set_xlabel('time (s)')
+
+spindlePlot(fs,filtered,31)
+plt.tight_layout()
+plt.show()
+
+'''
+# frequency spectrum plot
+Nf = len(x2)
+f = np.linspace(0,fs,Nf)
+spect = 20*np.log10(abs(sp.fft.fft(x2)))
+pltlen = Nf//4
+fig2 = plt.figure(figsize=(14,4))
+plt.plot(f[:pltlen],spect[:pltlen])
+plt.title("Power Spectral Density")
+plt.xlabel('frequency (Hz)')
+plt.ylabel('magnitude (dB)')
+#plt.show()
+
+
+# show stages
+y = y_pred[:]
+#y += [y[-1]] # add last string to list so last plot line shows
+x = list(range(0, len(y)*30, 30))
+ax2 = ax.twinx()
+ax2.step(x,y,where='post',color='red')
+ax2.set_ylabel('Sleep stage')
+plt.tight_layout()
+plt.show()
+'''
---
+++ src/testPlots.py
@@ -0,0 +1,183 @@
+import yasa
+import numpy as np
+from pathlib import Path
+from scipy.io.wavfile import read
+from mne.filter import filter_data
+import matplotlib.pyplot as plt
+
+# This file defines functions for visualizing
+# yasa detection functions
+# much of this code was copied from 
+# yasa notebooks by Raphael Vallat
+# https://github.com/raphaelvallat/yasa/tree/master/notebooks
+
+def spindlePlot(fs,indata,epochNum):
+    title = 'spindles'
+
+    # extract specified epoch
+    start = 30 * epochNum * fs
+    stop = 30 * (epochNum + 1) * fs
+    data = indata[start:stop]
+    ts = 1/fs
+    N = len(data)
+    t = np.arange(0,N*ts,ts)
+
+    # detect sleep spindles
+    sp = yasa.spindles_detect(data,fs)
+    # check if sp detected spindles, if not, skip epoch
+    if sp is None:
+        print("No spindles detected")
+        return 1
+    # bool vector indicating for each sample
+    mask = sp.get_mask()
+
+    spindles_highlight = data * mask
+    spindles_highlight[spindles_highlight == 0] = np.nan
+
+    plt.figure(figsize=(14, 4))
+    plt.plot(t, data, 'k')
+    plt.plot(t, spindles_highlight, 'indianred')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Amplitude (uV)')
+    plt.xlim([0, t[-1]])
+    plt.title('N2 sleep EEG data (2 spindles detected)')
+    # plt.savefig('detection.png', dpi=300, bbox_inches='tight')
+
+def thresholdPlot(fs,indata,epochNum):
+    title = 'spindles'
+
+    # extract specified epoch
+    start = 30 * epochNum * fs
+    stop = 30 * (epochNum + 1) * fs
+    data = indata[start:stop]
+    ts = 1/fs
+    N = len(data)
+    t = np.arange(0,N*ts,ts)
+
+    # detect sleep spindles
+    sp = yasa.spindles_detect(data,fs)
+    #print(sp.summary())
+
+    # check if sp detected spindles, if not, skip epoch
+    if sp is None:
+        print("No spindles detected")
+        return 1
+
+    # assign peak spindle time to index array
+    spTime = sp.summary()['Peak']
+
+    # plot time series
+    fig1 = plt.figure(figsize=(14,4))
+    plt.plot(t,data)
+    # min,max values in array
+    lo,hi = [min(data),max(data)]
+    # plot red vertical lines at every spindle peak
+    for i in spTime:
+        plt.vlines(i, lo, hi, colors='r')
+    plt.title(f"{title} time series")
+    plt.xlabel('time (s)')
+    plt.ylabel('amplitude (uV)')
+    # save and close figure
+    plt.savefig(f"{title}_timeseries.png")
+    plt.close()
+
+    # threshold 1: sigma band detection
+    # Broadband (1 - 30 Hz) bandpass filter
+    freq_broad = (1, 30)
+    data_broad = filter_data(data, fs, freq_broad[0], freq_broad[1], method='fir',verbose=0)
+
+    # Compute the pointwise relative power using STFT and cubic interpolation
+    f, t, Sxx = yasa.stft_power(data_broad, fs, window=2, step=.2, band=freq_broad, norm=True, interp=True)
+
+    # Extract the relative power in the sigma band
+    idx_sigma = np.logical_and(f >= 11, f <= 16)
+    rel_pow = Sxx[idx_sigma].sum(0)
+
+    # Plot
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14, 8), sharex=True)
+    plt.subplots_adjust(hspace=.25)
+    im = ax1.pcolormesh(t, f, Sxx, cmap='Spectral_r', vmax=0.2)
+    ax1.set_title('Spectrogram')
+    ax1.set_ylabel('Frequency (Hz)')
+    ax2.plot(t, rel_pow)
+    ax2.set_ylabel('Relative power (% $uV^2$)')
+    ax2.set_xlim(t[0], t[-1])
+    ax2.set_xlabel('Time (sec)')
+    ax2.axhline(0.20, ls=':', lw=2, color='indianred', label='Threshold #1')
+    plt.legend()
+    _ = ax2.set_title('Relative power in the sigma band')
+    # save figure
+    #plt.savefig(f"{title}_sigma_rel_power.png")
+
+    # threshold 2: moving correlation
+    data_sigma = filter_data(data, fs, 12, 15, l_trans_bandwidth=1.5, 
+                         h_trans_bandwidth=1.5, method='fir', verbose=0)
+    
+    # window size 300ms, step size 100ms
+    t, mcorr = yasa.moving_transform(data_sigma, data_broad, fs, window=.3, step=.1, method='corr', interp=True)
+
+    plt.figure(figsize=(14, 4))
+    plt.plot(t, mcorr)
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Pearson correlation')
+    plt.axhline(0.65, ls=':', lw=2, color='indianred', label='Threshold #2')
+    plt.legend()
+    plt.title('Moving correlation between $EEG_{bf}$ and $EEG_{\sigma}$')
+    _ = plt.xlim(0, t[-1])
+    #plt.savefig(f"{title}_movcorrel.png")
+
+    # threshold 3: moving rms
+    t, mrms = yasa.moving_transform(data_sigma, data, fs, window=.3, step=.1, method='rms', interp=True)
+
+    # Define threshold
+    trimmed_std = yasa.trimbothstd(mrms, cut=0.025)
+    thresh_rms = mrms.mean() + 1.5 * trimmed_std
+
+    plt.figure(figsize=(14, 4))
+    plt.plot(t, mrms)
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Root mean square')
+    plt.axhline(thresh_rms, ls=':', lw=2, color='indianred', label='Threshold #3')
+    plt.legend()
+    plt.title('Moving RMS of $EEG_{\sigma}$')
+    _ = plt.xlim(0, data[-1])
+    #plt.savefig(f"{title}_movrms.png")
+
+def swPlot(fs,indata,epochNum,title):
+
+    # extract specified epoch
+    start = 30 * epochNum * fs
+    stop = 30 * (epochNum + 1) * fs
+    data = indata[start:stop]
+    ts = 1/fs
+    N = len(data)
+    t = np.arange(0,N*ts,ts)
+
+    sw = yasa.sw_detect(data, fs)
+    # check if sp detected spindles, if not, skip epoch
+    if sw is None:
+        print("No slow waves detected")
+        return 1
+    # To get the full detection dataframe, we use the .summary() method
+    events = sw.summary()
+    #print(events.round(2))
+
+    # mask indicating for each sample
+    mask = sw.get_mask()
+    
+    sw_highlight = data * mask
+    sw_highlight[sw_highlight == 0] = np.nan
+
+    plt.figure(figsize=(16, 4.5))
+    plt.plot(t, data, 'k')
+    plt.plot(t, sw_highlight, 'indianred')
+    plt.plot(events['NegPeak'], sw_highlight[(events['NegPeak'] * fs).astype(int)], 'bo', label='Negative peaks')
+    plt.plot(events['PosPeak'], sw_highlight[(events['PosPeak'] * fs).astype(int)], 'go', label='Positive peaks')
+    plt.plot(events['Start'], data[(events['Start'] * fs).astype(int)], 'ro', label='Start')
+
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Amplitude (uV)')
+    plt.xlim([0, t[-1]])
+    plt.title('sleep EEG data')
+    plt.legend()
+    #plt.savefig(f"{title}_slowwaves.png")
