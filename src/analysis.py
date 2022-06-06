d812169c43bd7700fd1704d3e1cf5ce712660c43
Julia Filipchuk <bfilipc2@pdx.edu>
2022-06-05 22:35:47 -0700

    Modify analysis.py for python<3.10, test input

--- src/analysis.py
+++ src/analysis.py
@@ -45,69 +45,66 @@ file_predictions = None
 file_probabilities = None
 
 for line in sys.stdin:
+    (cmd, filename) = line.split(line, maxsplit=1)
+    if cmd == "DONE":
+        if file_predictions:
+            file_predictions.close()
+            file_predictions = None
+        if file_probabilities:
+            file_probabilities.close()
+            file_probabilities = None
+        os.exit()
+    if cmd == "RESET":
+        print("Do Reset", file=sys.stderr)
 
-    match line.split():
+    if cmd == "FILE":
+        if __debug__:
+            print("retreiving file ", filename, file=sys.stderr)
+        # get directory to save data to
+        dirname = os.path.dirname(filename)
+        if file_predictions is None:
+            file_predictions = open(f'{dirname}/predictions.txt', 'w')
+        if file_probabilities is None:
+            file_probabilities = open(f'{dirname}/predictprob.txt', 'w')
 
-        case ["DONE"]:
-            if file_predictions:
-                file_predictions.close()
-                file_predictions = None
-            if file_probabilities:
-                file_probabilities.close()
-                file_probabilities = None
-            os.exit()
+        print(f'directory: {dirname}', file=sys.stderr)
+        # read file
+        name = Path(filename).stem
+        fs, epoch = read(filename)
+        # 5 min minimum length of data to analyse
+        Nmin = fs * 60 * 5
+        # nap parameters in samples
+        minNapSamples = fs * minNapMinutes * 60
+        # concat to fullData
+        fullData = np.concatenate((fullData,epoch), axis=None)
 
-        case ["RESET"]:
-            print("Do Reset", file=sys.stderr)
+        if len(fullData) < Nmin:
+            pass
+        else:
+            # build mne raw object
+            raw = buildRawFromArray(fs,fullData)
+            # apply yasa analysis
+            sls = yasa.SleepStaging(raw, eeg_name="Fz")
+            y_pred = sls.predict()
+            y_prob = sls.predict_proba()
+            # add array of predictions for this chunk to array of chunk predictions
+            #predictSets.append(y_pred)
+            #probSets.append(y_prob)
 
-        case ["FILE", filename]:
+            print(' '.join(y_pred).replace('W','W ').replace('R','R '), file=file_predictions)
+            print(y_prob, file=file_probabilities)
+
             if __debug__:
-                print("retreiving file ", filename, file=sys.stderr)
-            # get directory to save data to
-            dirname = os.path.dirname(filename)
-            if file_predictions is None:
-                file_predictions = open(f'{dirname}/predictions.txt', 'w')
-            if file_probabilities is None:
-                file_probabilities = open(f'{dirname}/predictprob.txt', 'w')
+                print(f"{len(fullData)/fs} seconds analyzed", file=sys.stderr)
 
-            print(f'directory: {dirname}', file=sys.stderr)
-            # read file
-            name = Path(filename).stem
-            fs, epoch = read(filename)
-            # 5 min minimum length of data to analyse
-            Nmin = fs * 60 * 5
-            # nap parameters in samples
-            minNapSamples = fs * minNapMinutes * 60
-            # concat to fullData
-            fullData = np.concatenate((fullData,epoch), axis=None)
-
-            if len(fullData) < Nmin:
-                pass
-            else:
-                # build mne raw object
-                raw = buildRawFromArray(fs,fullData)
-                # apply yasa analysis
-                sls = yasa.SleepStaging(raw, eeg_name="Fz")
-                y_pred = sls.predict()
-                y_prob = sls.predict_proba()
-                # add array of predictions for this chunk to array of chunk predictions
-                #predictSets.append(y_pred)
-                #probSets.append(y_prob)
-
-                print(' '.join(y_pred).replace('W','W ').replace('R','R '), file=file_predictions)
-                print(y_prob, file=file_probabilities)
-
-                if __debug__:
-                    print(f"{len(fullData)/fs} seconds analyzed", file=sys.stderr)
-
-            if len(fullData) > minNapSamples:
-                count = np.count_nonzero(y_pred[9:-6] == "N2")
-                print(f"counted {count} N2 stages", file=sys.stderr)
-                if count >= minN2epochs:
-                    print("WAKEUP")
-                    print("WAKEUP!", file=sys.stderr)
-                    break
-                elif len(y_pred) > maxNapEpochs:
-                    print("WAKEUP")
-                    print("Surpassed maximum sleep duration without reaching N2 threshold", file=sys.stderr)
-                    break
+        if len(fullData) > minNapSamples:
+            count = np.count_nonzero(y_pred[9:-6] == "N2")
+            print(f"counted {count} N2 stages", file=sys.stderr)
+            if count >= minN2epochs:
+                print("WAKEUP")
+                print("WAKEUP!", file=sys.stderr)
+                break
+            elif len(y_pred) > maxNapEpochs:
+                print("WAKEUP")
+                print("Surpassed maximum sleep duration without reaching N2 threshold", file=sys.stderr)
+                break
